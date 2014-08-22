930b27758b1b9bd44786d15f1d5646d4f6e37cff
Roger Fischer <roger.fischer@kaywa.com>
2014-08-22 07:40:30 +0200

    question 2 only

---
+++ part1_q2.rb
@@ -0,0 +1,5 @@
+# 2
+# -
+# Define a method max_2_sum which takes an array of integers as an argument and 
+# returns the sum of its two largest elements. For an empty array it should return zero.
+# For an array with just one element, it should return that element.
