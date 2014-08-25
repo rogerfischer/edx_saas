2852e4576d66bc76ee68fb41981e191c0c445d09
Roger Fischer <roger.fischer@kaywa.com>
2014-08-25 07:41:47 +0200

    part1_q3.rb

---
+++ part1_q3.rb
@@ -0,0 +1,5 @@
+# 3
+# -
+# Define a method sum_to_n? which takes an array of integers and an additional integer, n, as 
+# arguments and returns true if any two distinct elements in the array of integers sum to n. 
+# An empty array or single element array should both return false. 
